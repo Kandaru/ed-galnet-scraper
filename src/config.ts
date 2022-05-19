7f30ad18cbdfb21b03881fe24104a955a47887cd
Ivan Kazakov <ki@certit.ru>
2022-05-20 00:20:26 +0300

    Число подтягиваемых записей изменено на 10.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const scraper = {
-  articlesApiUrl: 'https://cms.zaonce.net/ru-RU/jsonapi/node/galnet_article?page[limit]=1&sort=-created',
+  articlesApiUrl: 'https://cms.zaonce.net/ru-RU/jsonapi/node/galnet_article?page[limit]=10&sort=-created',
   postTemplateUrl: 'https://www.elitedangerous.com/ru-RU/%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8/galnet/',
   imageTemplateUrl: 'https://hosting.zaonce.net/elite-dangerous/galnet/',
   delay: 5 * 60 * 1000
