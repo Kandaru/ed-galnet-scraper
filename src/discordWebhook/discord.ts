15d4621c2c6e0814015f2769c4849928d408c99d
Ivan Kazakov <ki@certit.ru>
2022-06-30 11:01:46 +0500

    Теперь если текст статьи превышает 4к символов, кол-во абзацев подрезается.

--- src/discordWebhook/discord.ts
+++ src/discordWebhook/discord.ts
@@ -18,30 +18,57 @@ export class DiscordWebhook {
   }
 
   public async sendWebhook(article: INewsArticle) {
+    const body = {
+      embeds: [
+        {
+          title: article.title,
+          description: this.createDescText(article.text),
+          url: article.url,
+          image: {
+            url: article.image
+          },
+          footer: {
+            text: 'Сделано мной на коленке для халявных новостей'
+          },
+          timestamp: moment(article.date, 'DD MMM YYYY').format(),
+          color: 15760133
+        }
+      ]
+    }
+
     const response = await fetch(this.url!, {
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify({
-        embeds: [
-          {
-            title: article.title,
-            description: article.text.join('\n'),
-            url: article.url,
-            image: {
-              url: article.image
-            },
-            footer: {
-              text: 'Сделано мной на коленке для халявных новостей'
-            },
-            timestamp: moment(article.date, 'DD MMM YYYY').format(),
-            color: 15760133
-          }
-        ]
-      })
+      body: JSON.stringify(body)
     });
 
+	if (!response.ok) {
+		console.log(response.status);
+		console.log(await response.text());
+    console.log(JSON.stringify(body, undefined, '\t'));
+	}
+
     return response.ok;
+  }
+
+  private createDescText(text: string[]) {
+    let result = '';
+    let tmp = '';
+
+    for (let pIndex = 0; pIndex < text.length; pIndex++) {
+      const paragraph = text[pIndex];
+
+      tmp += paragraph + '\n';
+      if (tmp.length > 4000) {
+        result += '(продолжение в оригинальной статье...)';
+        break;
+      }
+
+      result = tmp;
+    }
+
+    return result;
   }
 }
